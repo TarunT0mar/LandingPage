e672a288f7fa9850a5c9f88a7a9f8d33ecc0783e
TarunT0mar <tomartarun000@gmail.com>
2025-07-08 10:39:02 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Inria_Serif } from "next/font/google";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
